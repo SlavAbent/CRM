d3b2dc3bd38476e44537459c5c6a204e41a8828b
@abentss <abent@softlab.ru>
2024-05-19 20:04:30 +0500

    add pages and routes

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,11 @@
 import './index.scss';
 
-import { App } from './App';
-import { Provider } from 'react-redux';
+import { AppRoutes } from './AppRoutes';
 import ReactDOM from 'react-dom/client';
-import { store } from './store';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <Provider store={store}>
-    <App />
-  </Provider>
+  <AppRoutes />
 );
